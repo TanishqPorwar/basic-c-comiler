bb764c991482cf73f8c3df2792a308d66ae2c7e2
TanishqPorwar <tanishqporwar22@gmail.com>
2021-01-27 01:36:34 +0530

    initial commit

---
+++ 2-syntax-analyzer/testcases/test-case-4.c
@@ -0,0 +1,14 @@
+/*
+Test Case
+  - evaluate arithematic expressions
+*/
+int main()
+{
+  int c = 0, d, e, f, g;
+
+  c = c + 1; // c = 1
+  d = c * 5; // d = 5
+  e = d / 4; // e = 1
+  f = d % 3; // f = 2
+  g = d * f - e;
+}
---
+++ 4-intermediate code generator/test-func.c
@@ -0,0 +1,14 @@
+void fun2(char *s)
+{
+}
+
+void fun(int a, int b, int c)
+{
+}
+
+int main()
+{
+  int c;
+  c = 5;
+  fun(c, 2, 5);
+}
